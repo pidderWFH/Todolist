b421658f6cf53fa81afdcbf1eafa29ae6eadac62
Peter <guanwei0702@gmail.com>
2023-06-16 18:22:43 +0800

    fix: todolist.js logic

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // const pwShowHide = document.querySelectorAll(".hide-icon");
 // const links = document.querySelectorAll(".link");
 // login API
-const apiurl = "https://todoo.5xcamp.us";  
+const apiurl = "https://todoo.5xcamp.us";
 
 // 切換頁面
 // let currentPage = window.location.pathname;
@@ -16,59 +16,64 @@ const apiurl = "https://todoo.5xcamp.us";
 
 // 註冊 API
 const signup = document.querySelector(".js-signup");
-signup.addEventListener("click", (e) =>{
-    e.preventDefault();
-    const signupEmail = document.querySelector("#signup-email").value.trim();
-    const nickname = document.querySelector("#name").value.trim();
-    const signupPassword = document.querySelector("#password1").value.trim();
-    const checkedPassword = document.querySelector("#rePassword").value.trim();
-    if ( signupEmail === "" || nickname === "" || signupPassword === "" || checkedPassword === ""){
-        Swal.fire({
-            icon: "error",
-            title: "尚有未輸入欄位，請填寫",
-        });
-        console.log(signupEmail, nickname, signupPassword, checkedPassword);
-        return;
-    }else if ( signupPassword !== checkedPassword ){
-        Swal.fire({
-            icon: "error",
-            title: "密碼不一致 !!",
-        });
-        console.log(signupPassword, checkedPassword);
-        return;
-    }
-    axios.post(`${ apiurl }/users`, {
-        user: {
-            "email": signupEmail,
-            "nickname": nickname,
-            "password": signupPassword
-          }
+signup.addEventListener("click", (e) => {
+  e.preventDefault();
+  const signupEmail = document.querySelector("#signup-email").value.trim();
+  const nickname = document.querySelector("#name").value.trim();
+  const signupPassword = document.querySelector("#password1").value.trim();
+  const checkedPassword = document.querySelector("#rePassword").value.trim();
+  if (
+    signupEmail === "" ||
+    nickname === "" ||
+    signupPassword === "" ||
+    checkedPassword === ""
+  ) {
+    Swal.fire({
+      icon: "error",
+      title: "尚有未輸入欄位，請填寫",
+    });
+    console.log(signupEmail, nickname, signupPassword, checkedPassword);
+    return;
+  } else if (signupPassword !== checkedPassword) {
+    Swal.fire({
+      icon: "error",
+      title: "密碼不一致 !!",
+    });
+    // console.log(signupPassword, checkedPassword);
+    return;
+  }
+  axios
+    .post(`${apiurl}/users`, {
+      user: {
+        email: signupEmail,
+        nickname: nickname,
+        password: signupPassword,
+      },
     })
-    .then((res) =>{
-        axios.defaults.headers.common["Authorization"] = res.headers.authorization;
-        // console.log(res.data);
-        let token = res.headers.authorization;
-        let nickName = res.data.nickname;
-        localStorage.setItem("token", token);
-        localStorage.setItem("nickName", nickName);
-        Swal.fire({
-            icon: "success",
-            title: `${ nickName }，你好 !`
-        }).then(() =>{
-            window.location = location.href + "/todolist.html";
-            // window.location = "/Todolist/todolist.html";
-            // location.href
-        })
+    .then((res) => {
+      axios.defaults.headers.common["Authorization"] =
+        res.headers.authorization;
+      // console.log(res.data);
+      let token = res.headers.authorization;
+      let nickName = res.data.nickname;
+      localStorage.setItem("token", token);
+      localStorage.setItem("nickName", nickName);
+      Swal.fire({
+        icon: "success",
+        title: `${nickName}，你好 !`,
+      }).then(() => {
+        window.location.href = "todolist.html";
+      });
     })
-    .catch((err) =>{
-        console.log(err);
-        Swal.fire({
-            icon: "error",
-            text: err.response.data.message,
-            text: "帳號已被註冊，請重新註冊"
-        })
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      Swal.fire({
+        icon: "error",
+        text: err.response.data.message,
+        text: "帳號已被註冊，請重新註冊",
+      });
+    });
+});
 // function signUp(email, nickname, pwd){
 //     axios.post(`${apiurl}/users`,{
 //         "user": {
@@ -81,46 +86,50 @@ signup.addEventListener("click", (e) =>{
 //     .catch(error => console.log(error.response))
 // };
 
-
 // 登入 API
 const signin = document.querySelector(".js-signin");
-signin.addEventListener("click", (e) =>{
-    e.preventDefault();
-    const email = document.querySelector("#signin-email").value;
-    const password = document.querySelector("#password").value;
-    if( email === "" || password ===""){
-        Swal.fire({
-            icon: "error",
-            title: "尚有未輸入欄位，請填寫"
-        });
-        return;
-    }
-    axios.post(` ${ apiurl }/users/sign_in`, {
-        user:{
-            email,
-            password
-        }
+signin.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = document.querySelector("#signin-email").value;
+  const password = document.querySelector("#password").value;
+  if (email === "" || password === "") {
+    Swal.fire({
+      icon: "error",
+      title: "尚有未輸入欄位，請填寫",
+    });
+    return;
+  }
+  axios
+    .post(` ${apiurl}/users/sign_in`, {
+      user: {
+        email,
+        password,
+      },
     })
-    .then((res) =>{
-        // console.log(res.data);
-        axios.defaults.headers.common["Authorization"] = res.headers.authorization;
-        let token = res.headers.authorization;
-        let nickName = res.data.nickname;
-        localStorage.setItem("token", token);
-        localStorage.setItem("nickName", nickName);
-        Swal.fire({
-            icon: "success",
-            title: `${ nickName }，你好 !`
-        }).then(() =>{
-            // window.location = "../todolist.html";
-            window.location = "../todolist.html";
-        })
+    .then((res) => {
+      // console.log(res.data);
+      axios.defaults.headers.common["Authorization"] =
+        res.headers.authorization;
+      let token = res.headers.authorization;
+      let nickName = res.data.nickname;
+      localStorage.setItem("token", token);
+      localStorage.setItem("nickName", nickName);
+      Swal.fire({
+        icon: "success",
+        title: `${nickName}，你好 !`,
+      }).then(() => {
+        window.location.href = "todolist.html";
+      });
     })
-    .catch((err) =>{
-        console.log(err.response);
-        Swal.fire(` ${err.response.data.message} `, "帳號密碼錯誤或無此帳號", "error");
-    })
-})
+    .catch((err) => {
+      console.log(err.response);
+      Swal.fire(
+        ` ${err.response.data.message} `,
+        "帳號密碼錯誤或無此帳號",
+        "error"
+      );
+    });
+});
 
 // function signIn(email, pwd){
 //     axios.post(`${apiurl}/users/sign_in`,{
@@ -133,12 +142,10 @@ signin.addEventListener("click", (e) =>{
 //     .catch(error => console.log(error.response))
 // }
 
-
-
 // pwShowHide.forEach(hideIcon => {
 //     hideIcon.addEventListener("click", ()=>{
 //         let pwFields = hideIcon.parentElement.parentElement.querySelectorAll(".password");
-        
+
 //         pwFields.forEach(password =>{
 //             if(password.type === "password"){
 //                 password.type = "text";
@@ -159,44 +166,45 @@ signin.addEventListener("click", (e) =>{
 // })
 
 const pwShowHide = document.querySelectorAll("#pwdIcon");
-pwShowHide.forEach(hideIcon => {
-    hideIcon.addEventListener("click", ()=>{
-        let pwd = hideIcon.parentElement.parentElement.querySelectorAll("#password");
-        let pwd1 = hideIcon.parentElement.parentElement.querySelectorAll("#password1");
-        pwd.forEach(password =>{
-            if(password.type === "password"){
-                password.type = "text";
-                hideIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-                hideIcon.classList.replace("bx-show", "bx-hide");
-        })
-        pwd1.forEach(password =>{
-            if(password.type === "password"){
-                password.type = "text";
-                hideIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-                hideIcon.classList.replace("bx-show", "bx-hide");
-        })
-        
-        
-    })
-})
+pwShowHide.forEach((hideIcon) => {
+  hideIcon.addEventListener("click", () => {
+    let pwd =
+      hideIcon.parentElement.parentElement.querySelectorAll("#password");
+    let pwd1 =
+      hideIcon.parentElement.parentElement.querySelectorAll("#password1");
+    pwd.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        hideIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      hideIcon.classList.replace("bx-show", "bx-hide");
+    });
+    pwd1.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        hideIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      hideIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
 const repwdShowHide = document.querySelectorAll("#rePwdIcon");
-repwdShowHide.forEach(hideIcon =>{
-    hideIcon.addEventListener("click", ()=>{
-        let rePwd = hideIcon.parentElement.parentElement.querySelectorAll("#rePassword");
-        rePwd.forEach(password =>{
-            if(password.type === "password"){
-                password.type = "text";
-                hideIcon.classList.replace("bx-hide", "bx-show");
-                return;
-            }
-            password.type = "password";
-                hideIcon.classList.replace("bx-show", "bx-hide");
-        })
-    })
-})
+repwdShowHide.forEach((hideIcon) => {
+  hideIcon.addEventListener("click", () => {
+    let rePwd =
+      hideIcon.parentElement.parentElement.querySelectorAll("#rePassword");
+    rePwd.forEach((password) => {
+      if (password.type === "password") {
+        password.type = "text";
+        hideIcon.classList.replace("bx-hide", "bx-show");
+        return;
+      }
+      password.type = "password";
+      hideIcon.classList.replace("bx-show", "bx-hide");
+    });
+  });
+});
--- js/todolist.js
+++ js/todolist.js
@@ -1,34 +1,80 @@
-const apiurl = "https://todoo.5xcamp.us";  
+const apiurl = "https://todoo.5xcamp.us";
 const todoUser = document.querySelector(".todoUser");
 const btnLogout = document.querySelector(".logout");
+let data = [];
+todoUser.textContent = `${localStorage.getItem("nickName")} 的待辦`;
 
+todoUser.addEventListener("click", user);
 btnLogout.addEventListener("click", logout);
 
+// 使用者
+function user() {
+  window.location.reload();
+}
+
+// getTodo
+getTodos();
+// axios get Todo
+function getTodos() {
+  // let todos = {};
+  axios
+    .get(`${apiurl}/todos`, {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    })
+    .then((res) => {
+      document.querySelector(".inputTodo").value = "";
+      data = res.data.todos;
+      // console.log(res.data.todos);
+      // todos.data = res.data.content;
+      // todos.id = res.data.id;
+      renderTodos(data);
+      // console.log(data);
+    });
+}
 
 // 登出
-function logout(){
-    axios.delete(`${ apiurl }/users/sign_out`,{
-        headers: {
-            Authorization: localStorage.getItem("token")
-        }
-    }).then((res) =>{
-        Swal.fire({
-            icon: "success",
-            title: res.data.message,
-        }).then(() =>{
-            localStorage.clear();
-            window.location = "./index.html";
-            // window.location = "/Todolist/login.html";
-        })
-    }).catch((err) =>{
-        let errTxt = err.response.data.error ? error.response.data.err : "";
-        Swal.fire({
-            icon: "error",
-            title: err.response.data.message,
-            text: errTxt
-        })
+function logout() {
+  axios
+    .delete(`${apiurl}/users/sign_out`, {
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
     })
+    .then((res) => {
+      Swal.fire({
+        icon: "success",
+        title: res.data.message,
+      }).then(() => {
+        localStorage.clear();
+        window.location.href = "index.html";
+        // window.location = "/Todolist/login.html";
+      });
+    })
+    .catch((err) => {
+      let errTxt = err.response.data.error ? error.response.data.err : "";
+      Swal.fire({
+        icon: "error",
+        title: err.response.data.message,
+        text: errTxt,
+      });
+    });
 }
+
+// Tab changeTodo
+const todoTab = document.querySelector(".card-header");
+todoTab.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (todoTab) {
+    tabStatus = e.target.dataset.tab;
+    const elems = document.querySelectorAll(".card-header span");
+    elems.forEach((item) => item.classList.remove("active"));
+    e.target.classList.add("active");
+    // console.log(tabStatus);
+    renderTodos(data);
+  }
+});
 
 // if( window.localStorage.pathname === "todolist.html"){
 //     getTodos();
@@ -39,75 +85,63 @@ function logout(){
 //     todoUser.textContent = `${localStorage.getItem("nickName")} 的待辦`;
 //     window.location = "./todolist.html";
 // })
-// getTodo
-let data = [];
-// axios get Todo
-function getTodos(){
-    let todos = {};
-    axios.get(`${apiurl}/todos`,{
-        headers:{
-            Authorization : localStorage.getItem("token")
-        },
-    })
-    .then((res) => {
-        document.querySelector(".inputTodo").value = "";
-        data = res.data.todos;
-        // todos.data = res.data.content;
-        // todos.id = res.data.id;
-        renderTodos(data);
-        // console.log(data);
-    })
-}
+
 let tabStatus = "all";
 
 // 渲染畫面
-function renderTodos(data){
-    // console.log(data);
-    let showTodo = [];
-    if(tabStatus === "all"){
-        showTodo = data;
-    }else if(tabStatus === "undo"){
-        showTodo = data.filter((item) => item.completed_at === null);
-    }else if(tabStatus === "complete"){
-        showTodo = data.filter((item) => item.completed_at !== null);
-    }
-    
-    // console.log(showTodo);
-    let str = "";
-    showTodo.forEach(function(item){
-        str += `<li class="todos-item"  data-id="${ item.id }">
-        <div class="txt">
-            <i class="bx ${item.completed_at === null ? "bx-checkbox" : "bx-checkbox-checked" }"></i>
-            <span>
-                ${ item.content }
-            </span>
-        </div>
-        <div>
-            <i class="bx bxs-edit editTodo"></i>
-            <i class="bx bx-x delTodo"></i>
-        </div>
-    </li>`
-    });
-    todoUser.textContent = `${localStorage.getItem("nickName")} 的待辦`;
-    const todosList = document.querySelector(".todosList");
-    todosList.innerHTML = str;
-    // const todosContainer = document.querySelector(".card-todos");
-    // const undoNum = data.filter((item) => item.completed_at !== null).length;
-    // document.querySelector(".js-undoNum").textContent = undoNum;
-    // todosContainer.innerHTML = str;
-    undoCounter(data);
+function renderTodos(data) {
+  // console.log(data);
+  let showTodo = [];
+
+  if (tabStatus === "all") {
+    showTodo = data;
+  } else if (tabStatus === "undo") {
+    showTodo = data.filter((item) => item.completed_at === null);
+  } else if (tabStatus === "complete") {
+    showTodo = data.filter((item) => item.completed_at !== null);
+  }
+  // console.log(showTodo);
+
+  let str = "";
+  showTodo.forEach((item) => {
+    str += `<li class="todos-item"  data-id="${item.id}">
+          <div class="txt">
+              <i class="bx ${
+                item.completed_at === null
+                  ? "bx-checkbox"
+                  : "bx-checkbox-checked"
+              }"></i>
+              <span>
+                  ${item.content}
+              </span>
+          </div>
+          <div>
+              <i class="bx bxs-edit editTodo"></i>
+              <i class="bx bx-x delTodo"></i>
+          </div>
+      </li>`;
+  });
+  const todosList = document.querySelector(".todosList");
+  todosList.innerHTML = str;
+
+  // const todosContainer = document.querySelector(".card-todos");
+  // const undoNum = data.filter((item) => item.completed_at !== null).length;
+  // document.querySelector(".js-undoNum").textContent = undoNum;
+  // todosContainer.innerHTML = str;
+  undoCounter(data);
 }
 // 待完成事項
-function undoCounter(data){
-    let undoTotal = 0;
-    data.forEach(item =>{
-        if(item.completed_at === null ){ undoTotal++ };
-    })
-    const undoNum = document.querySelector(".js-undoNum");
-    undoNum.textContent = `${ undoTotal }`;
+function undoCounter(data) {
+  let undoTotal = 0;
+  data.forEach((item) => {
+    if (item.completed_at === null) {
+      undoTotal++;
+    }
+  });
+  const undoNum = document.querySelector(".js-undoNum");
+  undoNum.textContent = `${undoTotal}`;
 }
 // addTodo
-
 
 // 滑鼠點擊新增
 // const onMouse = document.querySelector(".addTodo").addEventListener("click", function(e){
@@ -123,36 +157,40 @@ onMouse.addEventListener("click", addTodo);
 //     }
 // });
 const onKeyboard = document.querySelector(".keyboardInput");
-onKeyboard.addEventListener("keyup", function(e){
-    if(e.which === 13){
-        addTodo();
-    }
+onKeyboard.addEventListener("keyup", function (e) {
+  if (e.which === 13) {
+    addTodo();
+  }
 });
 
 // axios add Todo
-function addTodo(){
-    const inputTxt = document.querySelector(".inputTodo").value;
-    // console.log(inputTxt.value);
-    axios.defaults.headers.common["Authorization"]= localStorage.getItem("token");
-    if(inputTxt.trim() === ""){
-        Swal.fire(`請輸入待辦事項`, "輸入內容為空", "warning")
-        return;
-    }
-    axios.post(`${apiurl}/todos`,{
-        "todo":{
-            "content": inputTxt
-        }
-    
+function addTodo() {
+  const inputTxt = document.querySelector(".inputTodo").value;
+  // console.log(inputTxt.value);
+  axios.defaults.headers.common["Authorization"] =
+    localStorage.getItem("token");
+  if (inputTxt.trim() === "") {
+    Swal.fire(`請輸入待辦事項`, "輸入內容為空", "warning");
+    return;
+  }
+
+  axios
+    .post(`${apiurl}/todos`, {
+      todo: {
+        content: inputTxt,
+      },
     })
     .then((res) => {
-        // console.log(res)
-        getTodos();
-        // let obj = {};
-        // obj.content =  inputTxt.value.trim();
-        // obj.check = "";
-        // data.unshift(obj);
+      // console.log(res)
+      getTodos();
+      if (tabStatus === "complete") {
+        tabStatus = "all";
+        const elems = document.querySelectorAll(".card-header span");
+        elems.forEach((item) => item.classList.remove("active"));
+        document.querySelector(".all").classList.add("active");
+      }
     })
-    .catch(err => console.log(err.response))
+    .catch((err) => console.log(err.response));
 }
 
 // function addTodo(){
@@ -160,96 +198,112 @@ function addTodo(){
 //     // 判斷 Input 是否為空
 //     if(inputTodo.trim().length === 0 ){
 //         Swal.fire("輸入錯誤", "待辦事項不能為空白", "warning")
-        
+
 //         return;
-//     } 
-        
+//     }
+
 //     let obj = {};
 //     obj.content = inputTodo;
 //     data.push(obj);
 //     renderTodos();
 
-    // 清空 Input
+// 清空 Input
 //     document.querySelector(".inputTodo").value = "";
 // }
 
 // todoList 全部
 const todoList = document.querySelector(".todosList");
-todoList.addEventListener("click", (e) =>{
-    e.preventDefault();
-    const target = e.target;
-    let todoId = e.target.closest("li").dataset.id;
-    if(target.classList.contains("delTodo")){
-        deleteTodo(todoId);
-    }else if(target.classList.contains("editTodo")){
-        editTodo(todoId);
-    }else{
-        toggleTodo(todoId);
-    }
-})
+todoList.addEventListener("click", (e) => {
+  e.preventDefault();
+  const target = e.target;
+  let todoId = target.closest("li").dataset.id;
+  index = data.findIndex((item) => item.id === todoId);
+  if (target.classList.contains("delTodo")) {
+    deleteTodo(todoId, index);
+  } else if (target.classList.contains("editTodo")) {
+    editTodo(todoId, index);
+  } else {
+    toggleTodo(todoId, index);
+  }
+});
 // 編輯 todo
-function editTodo(id){
-    Swal.fire({
-        icon: "info",
-        title: "請編輯內容",
-        input: "text"
-    }).then((content) =>{
-        if(content.value.trim() === undefined || content.value.trim() ==="" ){
-            Swal.fire("請輸入內容", "內容無法為空白，請重新輸入", "warning");
-            // return;
-        }
-    
-        axios.put(`${ apiurl }/todos/${ id }`,{
-            todo:{
-                "content": content.value
-            }
+function editTodo(id, index) {
+  Swal.fire({
+    icon: "info",
+    title: "請編輯內容",
+    input: "text",
+  }).then((content) => {
+    if (content.value === undefined || content.value.trim() === "") {
+      Swal.fire("請輸入內容", "內容無法為空白，請重新輸入", "warning");
+    } else {
+      axios.defaults.headers.common["Authorization"] =
+        localStorage.getItem("token");
+      axios
+        .put(`${apiurl}/todos/${id}`, {
+          todo: {
+            content: content.value,
+          },
         })
-        .then((res) =>{
-            getTodos();
-            // Swal.fire(`${ res.data.message }`, "編輯成功", "success")
+        .then((res) => {
+          // console.log(res);
+          data[index].content = res.data.content;
+          renderTodos(data);
+          // getTodos(data);
+          // Swal.fire(`編輯內容: ${res.data.content}`, "編輯成功", "success");
         })
-        .catch((err) =>{
-            console.log(err);
-        })
-    })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
 }
 
 // deleteTodo
-function deleteTodo(id){
-    // console.log(data);
-    axios.defaults.headers.common["Authorization"]= localStorage.getItem("token");
-    axios.delete(`${ apiurl }/todos/${ id }`)
-    .then((res) =>{
-        // Swal.fire(`${ res.data.message }`, "刪除成功", "success")
-        getTodos();
+function deleteTodo(id, index) {
+  // console.log(data);
+  data.splice(index, 1);
+  axios.defaults.headers.common["Authorization"] =
+    localStorage.getItem("token");
+  axios
+    .delete(`${apiurl}/todos/${id}`)
+    .then((res) => {
+      // Swal.fire(`${ res.data.message }`, "刪除成功", "success")
+      // getTodos();
+      renderTodos(data);
     })
-    .catch((err) =>{
-        console.log(err.response);
-    })
+    .catch((err) => {
+      console.log(err.response);
+    });
 }
 
 // delete all Todos
 const deleteAllTodos = document.querySelector(".js-deletAllTodos");
 deleteAllTodos.addEventListener("click", deleteTodos);
-function deleteTodos(e){
-    e.preventDefault();
-    axios.defaults.headers.common["Authorization"]= localStorage.getItem("token");
-    axios.get(`${ apiurl }/todos`)
-    .then((res) =>{
-        // filter 待完成( completed_at == null )的，就是已完成的
-        const completeTodo = res.data.todos.filter((item) => item.completed_at !== null);
-        const delAllUrl = completeTodo.map((item) => `${ apiurl }/todos/${ item.id }`);
+function deleteTodos(e) {
+  e.preventDefault();
+  axios.defaults.headers.common["Authorization"] =
+    localStorage.getItem("token");
+  axios.get(`${apiurl}/todos`).then((res) => {
+    // filter 待完成( completed_at == null )的，就是已完成的
+    const completeTodo = res.data.todos.filter(
+      (item) => item.completed_at !== null
+    );
+    const delAllUrl = completeTodo.map((item) => `${apiurl}/todos/${item.id}`);
 
-        Promise.all(delAllUrl.map((url) =>{
-            return axios.delete(url);
-        })).then((res) =>{
-            // console.log(res);
-            Swal.fire("刪除成功", "已完成項目已刪除", "success");
-            getTodos();
-        }).catch((err) =>{
-            console.log(err);
-        })
-    })
+    Promise.all(
+      delAllUrl.map((url) => {
+        return axios.delete(url);
+      })
+    )
+      .then((res) => {
+        // console.log(res);
+        Swal.fire("刪除成功", "已完成項目已刪除", "success");
+        getTodos();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 }
 
 // updateTodo
@@ -257,7 +311,7 @@ function deleteTodos(e){
 //     axios.defaults.headers.common["Authorization"]= localStorage.getItem("token");
 //     axios.put(`${ apiurl }/todos/${ id }`, {
 //         todos:{
-//             content: 
+//             content:
 //         }
 //     })
 //     .then((res) =>{
@@ -270,34 +324,24 @@ function deleteTodos(e){
 //     if(target.classList.contains("delTodo")){
 //         // target.parentNode.remove();
 //         console.log(target);
-        
+
 //     }
 // })
 
 // toggle Todo
-function toggleTodo(id){
-    
-    axios.defaults.headers.common["Authorization"]= localStorage.getItem("token");
-    axios.patch(`${ apiurl }/todos/${ id }/toggle`, {})
-    .then((res) =>{
-        getTodos();
+function toggleTodo(id, index) {
+  axios.defaults.headers.common["Authorization"] =
+    localStorage.getItem("token");
+  axios
+    .patch(`${apiurl}/todos/${id}/toggle`, {})
+    .then((res) => {
+      // console.log(id);
+      data[index].completed_at = res.data.completed_at;
+      renderTodos(data);
     })
-    .catch((err) =>{
-        let reason = error.response.data.error ? error.response.data.error : "";
-        alert(error.response.data.message + "" + reason)
-    })
+    .catch((err) => {
+      // let reason = err.response.data.error ? err.response.data.error : "";
+      // alert(err.response.data.message + "" + reason);
+      console.log(err);
+    });
 }
-
-// Tab changeTodo
-const todoTab = document.querySelector(".card-header"); 
-todoTab.addEventListener("click", (e) =>{
-    e.preventDefault();
-    if(todoTab){
-        tabStatus = e.target.dataset.tab;
-        const elems = document.querySelectorAll(".card-header span");
-        elems.forEach((item) => item.classList.remove("active"));
-        e.target.classList.add("active");
-        // console.log(tabStatus);
-        getTodos();
-    }
-})
